bc74334af6bab27ea1b778c9866d2a92d6d35501
Cláudio Aguiar <claudioaguiargomes@gmail.com>
2022-07-29 15:15:31 -0400

    Update remembermedicine.py

--- remembermedicine.py
+++ remembermedicine.py
@@ -1,21 +1,22 @@
 import datetime
 print(datetime.time())
-remedios = []
+remedios = {}
 
 quantidade_remedios = int(input('Quantos remédios você quer cadastrar? '))
 
 
 for q in range(1,quantidade_remedios+1):
-    remedio = str(input(f'Nome do {q}º remédio: ')).strip().lower()
-    horario = int(input('Que horas você tomou? [HORA CHEIA]'))
+    remedios[f'remedio{q}'] = str(input(f'Nome do {q}º remédio: ')).strip().lower()
+    remedios[f'horario{q}'] = int(input('Que horas você tomou? [HORA CHEIA]'))
     freq = int(input('Com que frequência você deve tomar o remédio? '))
-    prox_hora = datetime.time(horario).hour + datetime.time(freq).hour
-    dados = (remedio,horario,prox_hora)
-    remedios.append(dados)
-    print(f'Próximo horario para tomar o remédio: {prox_hora} Horas')
-    print(remedios)
+    if remedios[f'horario{q}'] + freq < 24:
+        remedios[f'prox_hora{q}'] = datetime.time(remedios[f'horario{q}']).hour + datetime.time(freq).hour
+    else:
+        remedios[f'prox_hora{q}'] = datetime.time((remedios[f'horario{q}']+freq)-24).hour
+
+print(remedios)
 
 print(f'{"Nº":<4}|{"Remédio":^18}|{"Horário":^10}|{"Próximo horário:":^20}')
-for c in range(1,len(remedios)+1):
-    print(f'{c}{remedios[0].title():^26}{remedios[1]:}H{remedios[2]:>14}H')
+for c in range(1,quantidade_remedios+1):
+    print(f'{c}{remedios[f"remedio{c}"]:^26}{remedios[f"horario{c}"]:}H{remedios[f"prox_hora{c}"]:>14}H')
 
